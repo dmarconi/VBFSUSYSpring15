e67dc8ebac5360c78b84f2c9f1f8a8261b85d2f5
Daniele <daniele.marconi87@gmail.com>
2016-09-07 16:40:06 +0200

    Fixed Normalization, rebinned pt plots

--- VBFSUSYanalyzer/tools/prospects_13TeV.C
+++ VBFSUSYanalyzer/tools/prospects_13TeV.C
@@ -12,7 +12,7 @@
 #include <fstream>
 #include <cmath>
 
-void var_plot(string variable, string label) {
+void var_plot(string variable, string label, int rebin) {
 	//loading input
 	TFile *inputfile_sig_1 = TFile::Open("prospects/VBFC1pmN2_C1ToTau_N2ToTauTau_LSP000_Stau295_Chargino300_1M.root");
 	TFile *inputfile_sig_2 = TFile::Open("prospects/VBFC1pmN2_C1ToTau_N2ToTauTau_LSP000_Stau195_Chargino200_1M.root");
@@ -30,7 +30,7 @@ void var_plot(string variable, string label) {
 
 	//woring on plots
 	TH1F* h_dijetinvariantmass_sig_1 = ((TH1F*)(inputfile_sig_1->Get(("demo/baselineObjectSelection/" + variable).c_str())));
-	h_dijetinvariantmass_sig_1->Scale(norm/h_dijetinvariantmass_sig_1->Integral("width"));
+	h_dijetinvariantmass_sig_1->Scale(norm/h_dijetinvariantmass_sig_1->Integral());
 	h_dijetinvariantmass_sig_1->SetTitle("CMS Simulation, 13 TeV");
 	h_dijetinvariantmass_sig_1->GetXaxis()->SetTitle((label).c_str());
 	h_dijetinvariantmass_sig_1->GetYaxis()->SetTitle("a.u.");
@@ -38,32 +38,35 @@ void var_plot(string variable, string label) {
 	h_dijetinvariantmass_sig_1->SetLineColor(7);
 	h_dijetinvariantmass_sig_1->SetFillStyle(3001);
 	h_dijetinvariantmass_sig_1->SetStats(kFALSE);
+	h_dijetinvariantmass_sig_1->Rebin(rebin);
 	temp_maximum = h_dijetinvariantmass_sig_1->GetMaximum();
 	temp_minimum = h_dijetinvariantmass_sig_1->GetMinimum();
 	if (temp_maximum > maximum) maximum = temp_maximum;
 	if ((temp_minimum < minimum) && (temp_minimum != 0.) && ((temp_minimum > 0.00001)) ) minimum = temp_minimum;
 
 	TH1F* h_dijetinvariantmass_sig_2 = ((TH1F*)(inputfile_sig_2->Get(("demo/baselineObjectSelection/" + variable).c_str())));
-	h_dijetinvariantmass_sig_2->Scale(norm/h_dijetinvariantmass_sig_2->Integral("width"));
+	h_dijetinvariantmass_sig_2->Scale(norm/h_dijetinvariantmass_sig_2->Integral());
 	h_dijetinvariantmass_sig_2->GetXaxis()->SetTitle((label).c_str());
 	h_dijetinvariantmass_sig_2->GetYaxis()->SetTitle("a.u.");
 	h_dijetinvariantmass_sig_2->SetFillColor(8);
 	h_dijetinvariantmass_sig_2->SetLineColor(8);
 	h_dijetinvariantmass_sig_2->SetFillStyle(3001);
 	h_dijetinvariantmass_sig_2->SetStats(kFALSE);
+	h_dijetinvariantmass_sig_2->Rebin(rebin);
 	temp_maximum = h_dijetinvariantmass_sig_2->GetMaximum();
 	temp_minimum = h_dijetinvariantmass_sig_2->GetMinimum();
 	if (temp_maximum > maximum) maximum = temp_maximum;
 	if ((temp_minimum < minimum) && (temp_minimum != 0.) && ((temp_minimum > 0.00001)) ) minimum = temp_minimum;
 
 	TH1F* h_dijetinvariantmass_sig_3 = ((TH1F*)(inputfile_sig_3->Get(("demo/baselineObjectSelection/" + variable).c_str())));
-	h_dijetinvariantmass_sig_3->Scale(norm/h_dijetinvariantmass_sig_3->Integral("width"));
+	h_dijetinvariantmass_sig_3->Scale(norm/h_dijetinvariantmass_sig_3->Integral());
 	h_dijetinvariantmass_sig_3->GetXaxis()->SetTitle((label).c_str());
 	h_dijetinvariantmass_sig_3->GetYaxis()->SetTitle("a.u.");
 	h_dijetinvariantmass_sig_3->SetFillColor(9);
 	h_dijetinvariantmass_sig_3->SetLineColor(9);
 	h_dijetinvariantmass_sig_3->SetFillStyle(3001);
 	h_dijetinvariantmass_sig_3->SetStats(kFALSE);
+	h_dijetinvariantmass_sig_3->Rebin(rebin);
 	temp_maximum = h_dijetinvariantmass_sig_3->GetMaximum();
 	temp_minimum = h_dijetinvariantmass_sig_3->GetMinimum();
 	if (temp_maximum > maximum) maximum = temp_maximum;
@@ -72,11 +75,12 @@ void var_plot(string variable, string label) {
 	TH1F* h_dijetinvariantmass_qcd = ((TH1F*)(inputfile_qcd->Get(("demo/baselineObjectSelection/" + variable).c_str())));
 	h_dijetinvariantmass_qcd->SetLineColor(kRed);
 	h_dijetinvariantmass_qcd->SetLineWidth(3);
-	h_dijetinvariantmass_qcd->Scale(norm/h_dijetinvariantmass_qcd->Integral("width"));
+	h_dijetinvariantmass_qcd->Scale(norm/h_dijetinvariantmass_qcd->Integral());
 	h_dijetinvariantmass_qcd->SetTitle("");
 	h_dijetinvariantmass_qcd->GetXaxis()->SetTitle((label).c_str());
 	h_dijetinvariantmass_qcd->GetYaxis()->SetTitle("a.u.");
 	h_dijetinvariantmass_qcd->SetStats(kFALSE);
+	h_dijetinvariantmass_qcd->Rebin(rebin);
 	temp_maximum = h_dijetinvariantmass_qcd->GetMaximum();
 	temp_minimum = h_dijetinvariantmass_qcd->GetMinimum();
 	if (temp_maximum > maximum) maximum = temp_maximum;
@@ -85,11 +89,12 @@ void var_plot(string variable, string label) {
 	TH1F* h_dijetinvariantmass_vv = ((TH1F*)(inputfile_vv->Get(("demo/baselineObjectSelection/" + variable).c_str())));
 	h_dijetinvariantmass_vv->SetLineColor(kBlack);
 	h_dijetinvariantmass_vv->SetLineWidth(3);
-	h_dijetinvariantmass_vv->Scale(norm/h_dijetinvariantmass_vv->Integral("width"));
+	h_dijetinvariantmass_vv->Scale(norm/h_dijetinvariantmass_vv->Integral());
 	h_dijetinvariantmass_vv->SetTitle("");
 	h_dijetinvariantmass_vv->GetXaxis()->SetTitle((label).c_str());
 	h_dijetinvariantmass_vv->GetYaxis()->SetTitle("a.u.");
 	h_dijetinvariantmass_vv->SetStats(kFALSE);
+	h_dijetinvariantmass_vv->Rebin(rebin);
 	temp_maximum = h_dijetinvariantmass_vv->GetMaximum();
 	temp_minimum = h_dijetinvariantmass_vv->GetMinimum();
 	if (temp_maximum > maximum) maximum = temp_maximum;
@@ -99,11 +104,12 @@ void var_plot(string variable, string label) {
 	TH1F* h_dijetinvariantmass_wjets = ((TH1F*)(inputfile_wjets->Get(("demo/baselineObjectSelection/" + variable).c_str())));
 	h_dijetinvariantmass_wjets->SetLineColor(kBlue);
 	h_dijetinvariantmass_wjets->SetLineWidth(3);
-	h_dijetinvariantmass_wjets->Scale(norm/h_dijetinvariantmass_wjets->Integral("width"));
+	h_dijetinvariantmass_wjets->Scale(norm/h_dijetinvariantmass_wjets->Integral());
 	h_dijetinvariantmass_wjets->SetTitle("");
 	h_dijetinvariantmass_wjets->GetXaxis()->SetTitle((label).c_str());
 	h_dijetinvariantmass_wjets->GetYaxis()->SetTitle("a.u.");
 	h_dijetinvariantmass_wjets->SetStats(kFALSE);
+	h_dijetinvariantmass_wjets->Rebin(rebin);
 	temp_maximum = h_dijetinvariantmass_wjets->GetMaximum();
 	temp_minimum = h_dijetinvariantmass_wjets->GetMinimum();
 	if (temp_maximum > maximum) maximum = temp_maximum;
@@ -146,10 +152,10 @@ void var_plot(string variable, string label) {
 
 //main function
 void prospects_13Tev (){
-	var_plot("h_dijetinvariantmass", "M_{(jet,jet)} [GeV]");
-	var_plot("h_tau1pt", "P_{t}(#tau_{1}) [GeV]");
-	var_plot("h_tau2pt", "P_{t}(#tau_{2}) [GeV]");
-	var_plot("h_jet1pt", "P_{t}(jet_{1}) [GeV]");
-	var_plot("h_met", "#slash{E}_{T} [GeV]");
+	var_plot("h_dijetinvariantmass", "M_{(jet,jet)} [GeV]", 1);
+	var_plot("h_tau1pt", "P_{t}(#tau_{1}) [GeV]", 5);
+	var_plot("h_tau2pt", "P_{t}(#tau_{2}) [GeV]", 5);
+	var_plot("h_jet1pt", "P_{t}(jet_{1}) [GeV]", 5);
+	var_plot("h_met", "#slash{E}_{T} [GeV]", 1);
 }
 
