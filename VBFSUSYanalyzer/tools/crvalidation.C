7d4462ea30dbfe86284ca59900570f6771453f2e
Daniele Marconi <daniele.marconi@desy.de>
2016-11-19 16:53:46 +0100

    Solved Yaxis problem, Added total statistical uncertainty
    Signal sample is still missing

--- VBFSUSYanalyzer/tools/crvalidation.C
+++ VBFSUSYanalyzer/tools/crvalidation.C
@@ -17,8 +17,7 @@
 void makeplot (string isoregion, string plotname) {
 
   //definition of stacking plotname
-  THStack *hs = new THStack("hs","Stacked 1D histograms");
-  hs->SetTitle("CMS Work in Progress");
+  THStack *hs = new THStack("hs","Stacked histograms");
 
   //opening background files
   TFile *allDY_ifile = TFile::Open("crvalidation/allDY.root");
@@ -33,6 +32,8 @@ void makeplot (string isoregion, string plotname) {
   TH1F* crplot_allTT;
   TH1F* crplot_allVV;
   TH1F* crplot_allWJets;
+  TH1F* crplot_allSum;
+  TH1F* crplot_allSum_err;
 
   crplot_allDY = ((TH1F*)(allDY_ifile->Get(("demo/" + isoregion +"ObjectSelection/" + plotname).c_str())));
   crplot_allDY->SetFillColor(594);
@@ -60,19 +61,50 @@ void makeplot (string isoregion, string plotname) {
   crplot_allWJets->SetLineColor(610);
   crplot_allWJets->SetStats(0);
 
+  //creating and rescaling the bkg sum plot
+  crplot_allSum = (TH1F*) crplot_allDY->Clone();
+  crplot_allSum->SetTitle("CMS Work in Progress");
+  crplot_allSum->Add(crplot_allQCD);
+  crplot_allSum->Add(crplot_allTT);
+  crplot_allSum->Add(crplot_allVV);
+  crplot_allSum->Add(crplot_allWJets);
+
+  double maximum = 0.;
+  double minimum = FLT_MAX;
+  int nbins = crplot_allSum->GetNbinsX();
+
+	for (int i = 0; i < nbins; i++) {
+    if ( (crplot_allSum->GetBinContent(i) < minimum) && (crplot_allSum->GetBinContent(i) != 0.) ) minimum = crplot_allSum->GetBinContent(i);
+    if ( crplot_allSum->GetBinContent(i) > maximum) maximum = crplot_allSum->GetBinContent(i);
+  }
+
+  crplot_allSum->GetYaxis()->SetRangeUser( (minimum * 0.1) , (maximum * 10.) );
+
+  //extra plot cosmetics
+  if ( plotname == "h_dijetinvariantmass" ) {
+    crplot_allSum->GetXaxis()->SetTitle("M_{(jet,jet)} [GeV]");
+  }
+
+  //defining errorbar plot
+  crplot_allSum_err = (TH1F*) crplot_allSum->Clone();
+  crplot_allSum_err->SetFillColor(kRed);
+  crplot_allSum_err->SetFillStyle(3002);
+  crplot_allSum_err->SetLineColor(kRed);
+
+
   //defining legend
-	TLegend* leg = new TLegend(0.58,0.7,0.9,0.9);
-	leg->SetTextSize(0.02);
+	TLegend* leg = new TLegend(0.70,0.65,0.9,0.9);
   leg->AddEntry(crplot_allDY,"Drell-Yan","f");
   leg->AddEntry(crplot_allQCD,"QCD","f");
   leg->AddEntry(crplot_allTT,"t#bar{t}","f");
   leg->AddEntry(crplot_allVV,"VV","f");
   leg->AddEntry(crplot_allWJets,"W + jets","f");
+  leg->AddEntry(crplot_allSum_err, "#sigma^{total}_{stat}");
 
   //creating canvas
-  TCanvas *my_canvas = new TCanvas;
+  TCanvas *my_canvas = new TCanvas("c1","c1",10,10,600,600);
+  //TCanvas *my_canvas = new TCanvas;
 	my_canvas->cd();
-	gPad->SetLogy();
 
   //stacking plots
   hs->Add(crplot_allDY);
@@ -80,15 +112,13 @@ void makeplot (string isoregion, string plotname) {
   hs->Add(crplot_allTT);
   hs->Add(crplot_allWJets);
   hs->Add(crplot_allQCD);
-  //gStyle->SetHistTopMargin(0.);
-  //hs->Draw("HIST");
-  //hs->SetMinimum(0.);
-  //hs->SetMaximum(10000000.);
-  //hs->Draw("HIST");
-  //hs->GetYaxis()->SetMoreLogLabels();
 
-  hs->Draw("HIST");
+  //painting and saving plots into canvas
+  crplot_allSum->Draw("HIST");
+  hs->Draw("HIST+same");
+  crplot_allSum_err->Draw("e2+same");
   leg->Draw();
+  gPad->SetLogy();
   my_canvas->Print(("results/" + plotname + "_" + isoregion +".pdf").c_str());
   my_canvas->Close();
 }
